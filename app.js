b9a712e9c00dd240373999acb31aa4b70aeafcd6
Jusselly Moreno <jusselly.moreno@balboadevelopers.com>
2019-05-09 13:26:05 -0500

    Fixed bugs

---
+++ app.js
@@ -0,0 +1,13 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const notifyServiceSid = process.env.TWILIO_NOTIFY_SERVICE_SID;
+const numbers = ['+xxxxxxxx'];
+const client = require('twilio')(accountSid, authToken);
+const opNumbers = numbers.map(number => {
+    return JSON.stringify({ binding_type: 'sms', address: number });
+ });
+client.notify
+  .services(notifyServiceSid)
+  .notifications.create({toBinding:opNumbers, body:'Your message to send.'})
+  .then(notification => console.log(notification.sid))
+  .catch(error => console.log(error));
