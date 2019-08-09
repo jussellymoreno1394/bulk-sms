9d9d4abdf47365debe373696305125362ebd7ec9
Jusselly Moreno <jmoreno@hypernovalabs.com>
2019-08-08 23:24:14 -0500

    Add .gitignore file and modify app.js file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const notifyServiceSid = process.env.TWILIO_NOTIFY_SERVICE_SID;
-const numbers = ['+xxxxxxxx'];
+const numbers = ['+########'];
 const client = require('twilio')(accountSid, authToken);
 const opNumbers = numbers.map(number => {
     return JSON.stringify({ binding_type: 'sms', address: number });
